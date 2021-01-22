7e6c1dbf6b4e13e901dd48a991bc9bef71ce4f85
DSikes <me@dsikes.com>
2021-01-22 16:24:24 -0500

    Restructured project slightly, more xaml wiring

---
+++ DropboxVirtualSync/Logistics/LazyLocalization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DropboxVirtualSync
+{
+    public class LazyLocalization
+    {
+        private MainWindow _mainWindow;
+
+        public LazyLocalization(MainWindow mainWindow)
+        {
+            AddTextBoxPrefillPerUsername();
+            _mainWindow = mainWindow;
+        }
+
+        private void AddTextBoxPrefillPerUsername()
+        {
+            var userName = Environment.UserName;
+
+            if (userName == "David")
+            {
+                _mainWindow.SourcePathTextBox.Text = @"\\w2k3nas1\EngData\Admin\Windows Common\Libraries";
+                _mainWindow.DestinationPathTextBox.Text = @"D:\Dropbox\Backups\TI Backups\Test Mirror";
+            }
+        }
+    }
+}
---
+++ DropboxVirtualSync/Views/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace DropboxVirtualSync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow //: Window
+    {
+        private readonly LazyLocalization _lazyLocalization;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _lazyLocalization = new LazyLocalization(this);
+        }
+
+        private void MirrorStartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MirrorStopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
