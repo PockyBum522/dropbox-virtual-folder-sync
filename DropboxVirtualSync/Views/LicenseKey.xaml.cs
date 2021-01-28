ddade2869e8af6bed4c13610a41e38731c8cef7d
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-01-27 21:35:35 -0500

    Add groupboxes around source/dest textboxes; adds menustrip and LicenseKey window

---
+++ DropboxVirtualSync/Views/LicenseKey.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DropboxVirtualSync.Views
+{
+    public partial class LicenseKey : Window
+    {
+        public LicenseKey()
+        {
+            InitializeComponent();
+        }
+
+        private void UseLicenseKey_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
